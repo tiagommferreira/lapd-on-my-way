5fa7f9daedcb7692286a1771ae920f8aac24e815
tiagommferreira <tiagommferreira55@gmail.com>
2016-04-26 15:44:21 +0100

    set user position

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 var pg = require('pg');
 var path = require("path");
+var xml2js = require('xml2js');
+
+app.use(bodyParser.json());
 var connectionString = process.env.DATABASE_URL || 'postgres://postgres:123123@localhost:5432/postgres';
 
 //root
@@ -70,20 +74,40 @@ app.get('/users/:id/location', function (request, response) {
 
 //set user location
 app.put('/users/:id/location', function (request, response) {
+
   pg.connect(connectionString, function(err, client, done) {
     //TODO: change query
+    var oldUser;
     client.query("SELECT data FROM users WHERE (xpath('//user/id/text()', data))[1]::text = ($1)", [request.params.id], function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
        {
-         var queryResponse = result.rows[0].data;
-         response.setHeader('Content-Type', 'text/xml');
-         response.send(queryResponse);
+         oldUser = result.rows[0].data;
+         xml2js.parseString(oldUser, function(err, result){
+           oldUserObj = result;
+         });
+
+         oldUserObj.user.position[0].latitude[0] = request.body.latitude;
+         oldUserObj.user.position[0].longitude[0] = request.body.longitude;
+
+         client.query("UPDATE users SET data = ($1) WHERE (xpath('//user/id/text()', data))[1]::text = ($2)", [new xml2js.Builder().buildObject(oldUserObj),request.params.id], function(err, result) {
+           done();
+           if (err)
+            { return console.error(err); }
+          else {
+            response.send("OK");
+          }
+         });
+
        }
     });
+
+
+
   });
+
 });
 
 
